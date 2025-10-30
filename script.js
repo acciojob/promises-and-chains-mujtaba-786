d4d037a97fed8c640331b8a72caa91459483a853
MOHD MUJTABA <56441628+mujtaba-786@users.noreply.github.com>
2025-10-30 19:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+let age = document.getElementById("age")
+let name1 = document.getElementById("name")
+let btn = document.getElementById("btn")
+btn.addEventListener('click', () => {
+     if(age.value.trim() == 0 || name1.value.trim().length == 0){
+         alert("Please enter valid details.")
+     }
+     else{
+    let newPromise = new Promise((resolve,reject)=>{
+         setTimeout(() => {
+          let a = Number.parseInt(age.value.trim())
+             if(a>18){
+              
+              resolve("Welcome, . You can vote.");
+             }
+             else{
+              
+              reject("Oh sorry . You aren't old enough.");
+             }
+         }, 4000);
+    });
+
+    newPromise
+    .then((msg)=>{
+      alert(msg);
+    })
+    .catch((err)=>{
+      alert(err);
+    });
+}})
